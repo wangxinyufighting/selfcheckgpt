50f7517e5d0c44b3ad0dc8f727354e33b286f66b
wangxinyufighting <wxy.782226002@qq.com>
2024-06-09 21:11:47 +0800

    fix bug

--- selfcheckgpt/modeling_selfcheck.py
+++ selfcheckgpt/modeling_selfcheck.py
@@ -268,25 +268,26 @@ class SelfCheckBERTScore:
         num_sentences = len(sentences)
         num_samples = len(sampled_passages)
         bertscore_array = np.zeros((num_sentences, num_samples))
+        
         for s in range(num_samples):
             sample_passage = sampled_passages[s]
             sentences_sample = [sent for sent in self.nlp(sample_passage).sents] # List[spacy.tokens.span.Span]
             sentences_sample = [sent.text.strip() for sent in sentences_sample if len(sent) > 3]
             num_sentences_sample  = len(sentences_sample)
-
-            refs  = expand_list1(sentences, num_sentences_sample) # r1,r1,r1,....
-            cands = expand_list2(sentences_sample, num_sentences) # s1,s2,s3,...
-
-            P, R, F1 = bert_score.score(
-                    cands, refs,
-                    lang=self.default_model, verbose=False,
-                    rescale_with_baseline=self.rescale_with_baseline,
-            )
-            F1_arr = F1.reshape(num_sentences, num_sentences_sample)
-            F1_arr_max_axis1 = F1_arr.max(axis=1).values
-            F1_arr_max_axis1 = F1_arr_max_axis1.numpy()
-
-            bertscore_array[:,s] = F1_arr_max_axis1
+            if num_sentences_sample > 0:
+                refs  = expand_list1(sentences, num_sentences_sample) # r1,r1,r1,....
+                cands = expand_list2(sentences_sample, num_sentences) # s1,s2,s3,...
+    
+                P, R, F1 = bert_score.score(
+                        cands, refs,
+                        lang=self.default_model, verbose=False,
+                        rescale_with_baseline=self.rescale_with_baseline,
+                )
+                F1_arr = F1.reshape(num_sentences, num_sentences_sample)
+                F1_arr_max_axis1 = F1_arr.max(axis=1).values
+                F1_arr_max_axis1 = F1_arr_max_axis1.numpy()
+    
+                bertscore_array[:,s] = F1_arr_max_axis1
 
         bertscore_mean_per_sent = bertscore_array.mean(axis=-1)
         one_minus_bertscore_mean_per_sent = 1.0 - bertscore_mean_per_sent
